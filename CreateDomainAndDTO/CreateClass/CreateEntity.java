668e96f13f4b338aa00838fc241f809ab03cf519
shoHasegawa28 <b1601732@planet.kanazawa-it.ac.jp>
2021-01-29 22:37:50 +0900

    エンティティ作成クラスの概要完成

--- CreateDomainAndDTO/CreateClass/CreateEntity.java
+++ CreateDomainAndDTO/CreateClass/CreateEntity.java
@@ -4,13 +4,21 @@ import Models.*;
 import java.util.*;
 
 public class CreateEntity extends AbstractCreateClass{
-	public CreateEntity(ArrayList<ClassSheed> sheeds){
+	
+	private String EntityName;
+
+	public CreateEntity(ArrayList<ClassSheed> sheeds,String entiyName){
 		super(sheeds);
+		EntityName = entiyName;
 	}
 
 	public void Create(){
+		System.out.println("public class "+ EntityName +" {");
+		System.out.println("");
 		for(ClassSheed sheed : Sheeds){
-			System.out.println(sheed.GetValiableName());
+			System.out.println("	public " + sheed.GetTypeNames() + " " + sheed.GetValiableName() + "{ get; private set;}");
+			System.out.println("");
 		}
+		System.out.println("}");
 	}
 }
--- CreateDomainAndDTO/Main.java
+++ CreateDomainAndDTO/Main.java
@@ -9,9 +9,14 @@ public class Main {
 		
 		OperateCsvFile test = new OperateCsvFile();
 		ArrayList<ClassSheed> sheeds =  test.Read();
+
+		System.out.print("エンティティ名を入力してください  ");
+		Scanner sc = new Scanner(System.in);
+		String entiyName = sc.nextLine();
+
 		ArrayList<AbstractCreateClass> classes = new ArrayList<AbstractCreateClass>(){ 
 			{
-				add(new CreateEntity(sheeds));
+				add(new CreateEntity(sheeds,entiyName));
 			} 
 		};
 		
